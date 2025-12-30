9c64253578e2d5397c5fc66fa4179e079390469b
librado <librado.cruzmx@gmail.com>
2025-12-30 08:15:50 -0800

    Update CORS configuration to include additional origin

--- bun-server-michoscribe/src/index.ts
+++ bun-server-michoscribe/src/index.ts
@@ -20,7 +20,7 @@ app.use('*', logger());
 app.use(
   '*',
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000' , 'http://localhost:3001', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:3000' , 'http://localhost:3001', 'https://michoscribe.mailcat.cloud', 'http://127.0.0.1:5173'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Authorization'],
     exposeHeaders: ['Content-Length'],
